be2a8a4cc1f8f45f35a433e266499df7ff410373
Maurice Bagalwa <57249405+MauriceBagalwa@users.noreply.github.com>
2021-10-19 10:06:50 +0100

    test second

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <h1> Hello wolrd, nous sommes mardi est no comment </h1>
+        <h1> Hello wolrd, nous sommes mardi </h1>
       </header>
     </div>
   );
